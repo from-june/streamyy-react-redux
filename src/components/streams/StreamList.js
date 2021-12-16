04cc326ff5886ecbf5b9bdc8c22beb500343756a
Juun Pak <june.mintlatte@gmail.com>
2021-12-16 19:39:02 +0900

    ✨ Implement to fetch a stream list

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreamList } from 'actions';
 
-const StreamList = () => <h1>StreamList</h1>;
+const StreamList = ({ fetchStreamList, streams }) => {
+  useEffect(() => {
+    fetchStreamList();
+  }, [fetchStreamList]);
 
-export default StreamList;
+  return (
+    <div className="ui segment">
+      <h2 className="ui header">Stream List</h2>
+      <div class="ui clearing divider"></div>
+      <div className="ui divided items">
+        {Object.values(streams).map(stream => (
+          <div className="item" key={stream.id}>
+            <i className="play circle icon large" />
+            <div className="middle aligned content">
+              <p className="header">{stream.title}</p>
+              <p className="description">{stream.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return { streams: state.streams };
+};
+
+export default connect(mapStateToProps, { fetchStreamList })(StreamList);

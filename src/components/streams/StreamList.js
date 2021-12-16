588e1e932b151036fb7040cb4d3a22f4c41efb71
Juun Pak <june.mintlatte@gmail.com>
2021-12-17 08:31:14 +0900

    ⚡️ Add a create stream button

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchStreamList } from 'actions';
+import { Link } from 'react-router-dom';
 
 const StreamList = ({ fetchStreamList, streams, currentUserId }) => {
   useEffect(() => {
@@ -14,30 +15,37 @@ const StreamList = ({ fetchStreamList, streams, currentUserId }) => {
           style={{ textAlign: 'right', marginTop: 'auto' }}
           className="right floated content"
         >
-          <button className="ui button basic primary">EDIT</button>
-          <button className="ui button basic negative">DELETE</button>
+          <button className="ui button primary basic tiny">EDIT</button>
+          <button className="ui button red basic tiny">DELETE</button>
         </div>
       );
     }
   };
 
   return (
-    <div className="ui segment">
-      <h2 className="ui header">Stream List</h2>
-      <div className="ui clearing divider"></div>
-      <div className="ui divided items">
-        {Object.values(streams).map(stream => (
-          <div className="item" key={stream.id}>
-            <i className="large middle aligned play circle icon" />
-            <div className="middle aligned content">
-              <p className="header">{stream.title}</p>
-              <p className="description">{stream.description}</p>
+    <>
+      <div className="ui segment">
+        <h2 className="ui header">Stream List</h2>
+        <div className="ui clearing divider"></div>
+        <div className="ui divided items">
+          {Object.values(streams).map(stream => (
+            <div className="item" key={stream.id}>
+              <i className="large middle aligned play circle icon" />
+              <div className="middle aligned content">
+                <p className="header">{stream.title}</p>
+                <p className="description">{stream.description}</p>
+              </div>
+              {renderAdmin(stream.userId)}
             </div>
-            {renderAdmin(stream.userId)}
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+      <div style={{ textAlign: 'right' }}>
+        <Link to="/streams/new" className="ui inverted button secondary">
+          Create Stream
+        </Link>
+      </div>
+    </>
   );
 };
 

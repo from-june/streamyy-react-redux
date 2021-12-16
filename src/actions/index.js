28fe1dc50bd63c3442ad39d657906a3e993feb4e
Juun Pak <june.mintlatte@gmail.com>
2021-12-16 18:32:03 +0900

    📝 Create bulk of action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,13 @@
-import { SIGN_IN, SIGN_OUT } from 'actions/types';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAM_LIST,
+  EDIT_STREAM,
+  DELETE_STREAM,
+  FETCH_STREAM
+} from 'actions/types';
+import { streams } from 'apis/streams';
 
 export const signIn = userId => {
   return {
@@ -11,4 +20,34 @@ export const signOut = () => {
   return {
     type: SIGN_OUT
   };
+};
+
+export const createStream = formValue => async dispatch => {
+  const { data } = await streams.post('/streams', formValue);
+
+  dispatch({ type: CREATE_STREAM, payload: data });
+};
+
+export const fetchStreamList = () => async dispatch => {
+  const { data } = await streams.get('/streams');
+
+  dispatch({ type: FETCH_STREAM_LIST, payload: data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const { data } = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: data });
+};
+
+export const editStream = (id, formValue) => async dispatch => {
+  const { data } = await streams.put(`/streams/${id}`, formValue);
+
+  dispatch({ type: EDIT_STREAM, payload: data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };

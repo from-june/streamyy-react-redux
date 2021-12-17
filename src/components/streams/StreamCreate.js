59b1346d84ab18066edd2d0bc83a5d32877c3bc9
Juun Pak <june.mintlatte@gmail.com>
2021-12-17 11:01:25 +0900

    ♻️ Import StreamForm to StreamCreate component

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { createStream } from 'actions';
 import { useForm } from 'react-hook-form';
+import { connect } from 'react-redux';
+import StreamForm from 'components/streams/StreamForm';
+import { createStream } from 'actions';
 
 const StreamCreate = ({ createStream }) => {
   const navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setValue
-  } = useForm();
+  const { setValue } = useForm();
 
   const onSubmit = data => {
     console.log(data);
@@ -22,35 +18,7 @@ const StreamCreate = ({ createStream }) => {
     navigate('/');
   };
 
-  return (
-    <form className="ui form error" onSubmit={handleSubmit(onSubmit)}>
-      <div className={`field ${errors.title ? 'error' : ''}`}>
-        <label htmlFor="title">Enter Title</label>
-        <input
-          id="title"
-          {...register('title', {
-            required: true
-          })}
-        />
-        {errors.title && (
-          <p className="ui pointing red basic label">Please enter a title.</p>
-        )}
-      </div>
-      <div className={`field ${errors.description ? 'error' : ''}`}>
-        <label htmlFor="description">Enter Description</label>
-        <input
-          id="description"
-          {...register('description', { required: true })}
-        />
-        {errors.description && (
-          <p className="ui pointing red basic label">
-            Please enter a description.
-          </p>
-        )}
-      </div>
-      <button className="ui button">Submit</button>
-    </form>
-  );
+  return <StreamForm onSubmit={onSubmit} />;
 };
 
 export default connect(null, { createStream })(StreamCreate);
